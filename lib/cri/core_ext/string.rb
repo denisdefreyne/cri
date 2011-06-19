7eaf2efe797b0889cb46059907b01c7f14804c1c
Denis Defreyne <denis.defreyne@stoneship.org>
2011-06-19 15:13:05 +0200

    improved string wrapping and indenting

--- lib/cri/core_ext/string.rb
+++ lib/cri/core_ext/string.rb
@@ -4,6 +4,22 @@ module Cri::CoreExtensions
 
   module String
 
+    # @todo Document
+    def to_paragraphs
+      lines = self.scan(/([^\n]+\n|[^\n]*$)/).map { |s| s[0].strip }
+
+      paragraphs = [ [] ]
+      lines.each do |line|
+        if line.empty?
+          paragraphs << []
+        else
+          paragraphs.last << line
+        end
+      end
+
+      paragraphs.reject { |p| p.empty? }.map { |p| p.join(' ') }
+    end
+
     # Word-wraps and indents the string.
     #
     # +width+:: The maximal width of each line. This also includes indentation,
@@ -12,7 +28,7 @@ module Cri::CoreExtensions
     # +indentation+:: The number of spaces to indent each wrapped line.
     def wrap_and_indent(width, indentation)
       # Split into paragraphs
-      paragraphs = self.split("\n").map { |p| p.strip }.reject { |p| p == '' }
+      paragraphs = self.to_paragraphs
 
       # Wrap and indent each paragraph
       paragraphs.map do |paragraph|
--- test/test_core_ext.rb
+++ test/test_core_ext.rb
@@ -2,22 +2,57 @@
 
 class Cri::CoreExtTestCase < Cri::TestCase
 
-  def test_string_wrap_and_indent
-    assert_equal(
-      "Lorem ipsum dolor sit amet, consectetur\n" +
-      "adipisicing elit, sed do eiusmod tempor\n" +
-      "incididunt ut labore et dolore magna\n" +
-      "aliqua.",
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.".wrap_and_indent(40, 0)
-    )
+  def test_string_to_paragraphs
+    original = "Lorem ipsum dolor sit amet,\nconsectetur adipisicing.\n\n" +
+               "Sed do eiusmod\ntempor incididunt ut labore."
 
-    assert_equal(
-      "    Lorem ipsum dolor sit amet,\n" +
-      "    consectetur adipisicing elit, sed\n" +
-      "    do eiusmod tempor incididunt ut\n" +
-      "    labore et dolore magna aliqua.",
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.".wrap_and_indent(36, 4)
-    )
+    expected = [ "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+                 "Sed do eiusmod tempor incididunt ut labore." ]
+
+    actual = original.to_paragraphs
+    assert_equal expected, actual
+  end
+
+  def test_string_wrap_and_indent_without_indent
+    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
+               "sed do eiusmod tempor incididunt ut labore et dolore " + 
+               "magna aliqua."
+
+    expected = "Lorem ipsum dolor sit amet, consectetur\n" +
+               "adipisicing elit, sed do eiusmod tempor\n" +
+               "incididunt ut labore et dolore magna\n" +
+               "aliqua."
+
+    actual = original.wrap_and_indent(40, 0)
+    assert_equal expected, actual
+  end
+
+  def test_string_wrap_and_indent_with_indent
+    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
+               "sed do eiusmod tempor incididunt ut labore et dolore " + 
+               "magna aliqua."
+
+    expected = "    Lorem ipsum dolor sit amet,\n" +
+               "    consectetur adipisicing elit, sed\n" +
+               "    do eiusmod tempor incididunt ut\n" +
+               "    labore et dolore magna aliqua."
+
+    actual = original.wrap_and_indent(36, 4)
+    assert_equal expected, actual
+  end
+
+  def test_string_wrap_and_indent_with_multiple_lines
+    original = "Lorem ipsum dolor sit\namet, consectetur adipisicing elit, " +
+               "sed do\neiusmod tempor incididunt ut\nlabore et dolore " + 
+               "magna\naliqua."
+
+    expected = "    Lorem ipsum dolor sit amet,\n" +
+               "    consectetur adipisicing elit, sed\n" +
+               "    do eiusmod tempor incididunt ut\n" +
+               "    labore et dolore magna aliqua."
+
+    actual = original.wrap_and_indent(36, 4)
+    assert_equal expected, actual
   end
 
 end
