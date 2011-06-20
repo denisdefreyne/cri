61d0eae16b8b3aecde1837c2878d9bff9eefa1cd
Denis Defreyne <denis.defreyne@stoneship.org>
2011-06-20 20:56:45 +0200

    bumped version to 2.0a3

--- lib/cri.rb
+++ lib/cri.rb
@@ -3,7 +3,7 @@
 module Cri
 
   # The current Cri version.
-  VERSION = '2.0a2'
+  VERSION = '2.0a3'
 
   autoload 'Command',           'cri/command'
   autoload 'CommandDSL',        'cri/command_dsl'
