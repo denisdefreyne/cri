09782a39024cb1abcde2e3113649458a83c8eb92
Denis Defreyne <denis.defreyne@stoneship.org>
2013-08-22 20:08:10 +0200

    Fix broken wrap-and-indent tests

--- test/test_core_ext.rb
+++ test/test_core_ext.rb
@@ -15,7 +15,7 @@ class Cri::CoreExtTestCase < Cri::TestCase
 
   def test_string_wrap_and_indent_without_indent
     original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-               "sed do eiusmod tempor incididunt ut labore et dolore " + 
+               "sed do eiusmod tempor incididunt ut labore et dolore " +
                "magna aliqua."
 
     expected = "Lorem ipsum dolor sit amet, consectetur\n" +
@@ -29,13 +29,14 @@ class Cri::CoreExtTestCase < Cri::TestCase
 
   def test_string_wrap_and_indent_with_indent
     original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-               "sed do eiusmod tempor incididunt ut labore et dolore " + 
+               "sed do eiusmod tempor incididunt ut labore et dolore " +
                "magna aliqua."
 
     expected = "    Lorem ipsum dolor sit amet,\n" +
-               "    consectetur adipisicing elit, sed\n" +
-               "    do eiusmod tempor incididunt ut\n" +
-               "    labore et dolore magna aliqua."
+               "    consectetur adipisicing elit,\n" +
+               "    sed do eiusmod tempor\n" +
+               "    incididunt ut labore et dolore\n" +
+               "    magna aliqua."
 
     actual = original.wrap_and_indent(36, 4)
     assert_equal expected, actual
@@ -43,7 +44,7 @@ class Cri::CoreExtTestCase < Cri::TestCase
 
   def test_string_wrap_and_indent_with_large_indent
     original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-               "sed do eiusmod tempor incididunt ut labore et dolore " + 
+               "sed do eiusmod tempor incididunt ut labore et dolore " +
                "magna aliqua."
 
     expected = "                              Lorem ipsum\n" +
@@ -65,13 +66,14 @@ class Cri::CoreExtTestCase < Cri::TestCase
 
   def test_string_wrap_and_indent_with_multiple_lines
     original = "Lorem ipsum dolor sit\namet, consectetur adipisicing elit, " +
-               "sed do\neiusmod tempor incididunt ut\nlabore et dolore " + 
+               "sed do\neiusmod tempor incididunt ut\nlabore et dolore " +
                "magna\naliqua."
 
     expected = "    Lorem ipsum dolor sit amet,\n" +
-               "    consectetur adipisicing elit, sed\n" +
-               "    do eiusmod tempor incididunt ut\n" +
-               "    labore et dolore magna aliqua."
+               "    consectetur adipisicing elit,\n" +
+               "    sed do eiusmod tempor\n" +
+               "    incididunt ut labore et dolore\n" +
+               "    magna aliqua."
 
     actual = original.wrap_and_indent(36, 4)
     assert_equal expected, actual
