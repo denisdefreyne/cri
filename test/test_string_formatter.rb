eb099972c23043a3d0647a552cc98a208c8e599c
Denis Defreyne <denis.defreyne@stoneship.org>
2015-01-17 10:56:30 +0100

    Fix Style/LineEndConcatenation

--- test/test_string_formatter.rb
+++ test/test_string_formatter.rb
@@ -6,7 +6,7 @@ class Cri::CoreExtTestCase < Cri::TestCase
   end
 
   def test_string_to_paragraphs
-    original = "Lorem ipsum dolor sit amet,\nconsectetur adipisicing.\n\n" +
+    original = "Lorem ipsum dolor sit amet,\nconsectetur adipisicing.\n\n" \
                "Sed do eiusmod\ntempor incididunt ut labore."
 
     expected = [ "Lorem ipsum dolor sit amet, consectetur adipisicing.",
@@ -17,13 +17,13 @@ class Cri::CoreExtTestCase < Cri::TestCase
   end
 
   def test_string_wrap_and_indent_without_indent
-    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-               "sed do eiusmod tempor incididunt ut labore et dolore " +
+    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " \
+               "sed do eiusmod tempor incididunt ut labore et dolore " \
                "magna aliqua."
 
-    expected = "Lorem ipsum dolor sit amet, consectetur\n" +
-               "adipisicing elit, sed do eiusmod tempor\n" +
-               "incididunt ut labore et dolore magna\n" +
+    expected = "Lorem ipsum dolor sit amet, consectetur\n" \
+               "adipisicing elit, sed do eiusmod tempor\n" \
+               "incididunt ut labore et dolore magna\n" \
                "aliqua."
 
     actual = formatter.wrap_and_indent(original, 40, 0)
@@ -31,14 +31,14 @@ class Cri::CoreExtTestCase < Cri::TestCase
   end
 
   def test_string_wrap_and_indent_with_indent
-    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-               "sed do eiusmod tempor incididunt ut labore et dolore " +
+    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " \
+               "sed do eiusmod tempor incididunt ut labore et dolore " \
                "magna aliqua."
 
-    expected = "    Lorem ipsum dolor sit amet,\n" +
-               "    consectetur adipisicing elit,\n" +
-               "    sed do eiusmod tempor\n" +
-               "    incididunt ut labore et dolore\n" +
+    expected = "    Lorem ipsum dolor sit amet,\n" \
+               "    consectetur adipisicing elit,\n" \
+               "    sed do eiusmod tempor\n" \
+               "    incididunt ut labore et dolore\n" \
                "    magna aliqua."
 
     actual = formatter.wrap_and_indent(original, 36, 4)
@@ -46,21 +46,21 @@ class Cri::CoreExtTestCase < Cri::TestCase
   end
 
   def test_string_wrap_and_indent_with_large_indent
-    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-               "sed do eiusmod tempor incididunt ut labore et dolore " +
+    original = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " \
+               "sed do eiusmod tempor incididunt ut labore et dolore " \
                "magna aliqua."
 
-    expected = "                              Lorem ipsum\n" +
-               "                              dolor sit\n" +
-               "                              amet,\n" +
-               "                              consectetur\n" +
-               "                              adipisicing\n" +
-               "                              elit, sed do\n" +
-               "                              eiusmod\n" +
-               "                              tempor\n" +
-               "                              incididunt ut\n" +
-               "                              labore et\n" +
-               "                              dolore magna\n" +
+    expected = "                              Lorem ipsum\n" \
+               "                              dolor sit\n" \
+               "                              amet,\n" \
+               "                              consectetur\n" \
+               "                              adipisicing\n" \
+               "                              elit, sed do\n" \
+               "                              eiusmod\n" \
+               "                              tempor\n" \
+               "                              incididunt ut\n" \
+               "                              labore et\n" \
+               "                              dolore magna\n" \
                "                              aliqua."
 
     actual = formatter.wrap_and_indent(original, 44, 30)
@@ -68,14 +68,14 @@ class Cri::CoreExtTestCase < Cri::TestCase
   end
 
   def test_string_wrap_and_indent_with_multiple_lines
-    original = "Lorem ipsum dolor sit\namet, consectetur adipisicing elit, " +
-               "sed do\neiusmod tempor incididunt ut\nlabore et dolore " +
+    original = "Lorem ipsum dolor sit\namet, consectetur adipisicing elit, " \
+               "sed do\neiusmod tempor incididunt ut\nlabore et dolore " \
                "magna\naliqua."
 
-    expected = "    Lorem ipsum dolor sit amet,\n" +
-               "    consectetur adipisicing elit,\n" +
-               "    sed do eiusmod tempor\n" +
-               "    incididunt ut labore et dolore\n" +
+    expected = "    Lorem ipsum dolor sit amet,\n" \
+               "    consectetur adipisicing elit,\n" \
+               "    sed do eiusmod tempor\n" \
+               "    incididunt ut labore et dolore\n" \
                "    magna aliqua."
 
     actual = formatter.wrap_and_indent(original, 36, 4)
