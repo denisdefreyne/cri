a3be5d1173dc1aa74ff411a8ba609bc189c1a0ca
Denis Defreyne <denis.defreyne@stoneship.org>
2015-01-17 11:35:47 +0100

    Fix Style/FormatString

--- lib/cri/help_renderer.rb
+++ lib/cri/help_renderer.rb
@@ -79,7 +79,7 @@ module Cri
       # Command
       shown_subcommands.sort_by { |cmd| cmd.name }.each do |cmd|
         text <<
-          sprintf(
+          format(
             "    %-#{length + 4}s %s\n",
             fmt.format_as_command(cmd.name, @io),
             cmd.summary)
